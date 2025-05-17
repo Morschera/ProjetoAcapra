267562a615e2cbd608e3755f96245ab17fb953d1
Arthur <arthur.debatin@unifebe.edu.br>
2025-05-16 21:12:57 -0300

    Adicionando botão de voltar

--- app/src/main/java/br/edu/unifebe/projeto20/FormularioActivity.kt
+++ app/src/main/java/br/edu/unifebe/projeto20/FormularioActivity.kt
@@ -1,11 +1,22 @@
 package br.edu.unifebe.projeto20
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class FormularioActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId", "WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.formulario_view)
+
+        val btnVoltar = findViewById<Button>(R.id.btnVoltar)
+        btnVoltar.setOnClickListener {
+            finish()
+        }
+
     }
 }
